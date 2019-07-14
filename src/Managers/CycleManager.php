403f0a7dc7741e649261de929531cb0de2ccfb77
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/CycleManager.php
+++ src/Managers/CycleManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Cycle newEntity()
+ * @method \Amethyst\Schemas\CycleSchema getSchema()
+ * @method \Amethyst\Repositories\CycleRepository getRepository()
+ * @method \Amethyst\Serializers\CycleSerializer getSerializer()
+ * @method \Amethyst\Validators\CycleValidator getValidator()
+ * @method \Amethyst\Authorizers\CycleAuthorizer getAuthorizer()
+ */
 class CycleManager extends Manager
 {
     use ConfigurableManager;
