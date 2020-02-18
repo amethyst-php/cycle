33835f5c0651cb542bbecb9b989e668f30668ec9
Railken <railken@protonmail.ch>
2020-02-18 19:11:54 +0100

    refactor morphto

--- src/Schemas/CycleSchema.php
+++ src/Schemas/CycleSchema.php
@@ -20,12 +20,12 @@ class CycleSchema extends Schema
             Attributes\EnumAttribute::make('started_status', config('amethyst.cycle.data.cycle.attributes.started_status.options')),
             Attributes\DateTimeAttribute::make('ended_at'),
             Attributes\EnumAttribute::make('ended_status', config('amethyst.cycle.data.cycle.attributes.ended_status.options')),
-            Attributes\EnumAttribute::make('cyclable_type', app('amethyst')->getMorphListable('cycle', 'cyclable'))
+            Attributes\EnumAttribute::make('cyclable_type', app('amethyst')->getDataNames())
                 ->setRequired(true),
             Attributes\MorphToAttribute::make('cyclable_id')
                 ->setRelationKey('cyclable_type')
                 ->setRelationName('cyclable')
-                ->setRelations(app('amethyst')->getMorphRelationable('cycle', 'cyclable'))
+                ->setRelations(app('amethyst')->getDataManagers())
                 ->setRequired(true),
             Attributes\CreatedAtAttribute::make(),
             Attributes\UpdatedAtAttribute::make(),
